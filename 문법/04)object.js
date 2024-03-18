b5a8ec0c8f469ed2c9bd43c812d6381780dc0167
minseo-o <halia0709@gmail.com>
2024-03-18 11:04:12 +0900

    속성 추가, 삭제

--- 문법/04)object.js
+++ 문법/04)object.js
@@ -1,11 +1,17 @@
 const cathy = {
     name: "cathy",
-    //속성 이름은 따옴표 처리 안해도 된다.
-    age :19,
-    skills:["자바스크립트", "파이썬", "코볼"]
+    age: 19,
+    skills: ["자바스크립트", "파이썬", "코볼"]
 }
 
 console.log(cathy)
 console.log(cathy.name)
 console.log(cathy["name"])
 
+// 속성 추가
+cathy["hobby"] = "독서"
+console.log(cathy)
+
+// 속성 제거
+delete cathy["hobby"]
+console.log(cathy)
